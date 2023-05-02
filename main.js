1f04a643f8363cca0e616dea79a9bfd76ef3b42a
Reem99-sina <reemsina2@gmail.com>
2023-05-03 00:10:44 +0300

    map

---
+++ main.js
@@ -0,0 +1,16 @@
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.120850),
+        zoom: 5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+myMap()
+var marker = new google.maps.Marker({
+    position: myCenter,
+    icon: 'pinkball.png'
+});
+
+marker.setMap(map);
+
+// marker.setMap(map);
